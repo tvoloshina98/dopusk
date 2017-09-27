79676fd6a572e0b2503ba7127e26c9fa1c8dc88f
Tatyana Voloshina <tvoloshina98@gmail.com>
2017-09-27 21:45:29 +0700

    Create solution square equlation

---
+++ sqr.h
@@ -0,0 +1,10 @@
+#ifndef KV_H
+#define KV_H
+#define FOUND_2_ROOT 2
+#define FOUND_1_ROOT 1
+#define ROOT_NOT_FOUND 0
+#define INVALUD_ARGUMENTS -1
+	
+int sqr(double a, double b, double c, double *x1, double *x2);
+	
+#endif
